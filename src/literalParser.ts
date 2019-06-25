876d0da961ebc228e442cf747f97776a2152e3ec
Martin Štekl <martin.stekl@gmail.com>
2019-06-25 08:35:31 +0200

    simplify literal parser to throw type errors

--- src/literalParser.ts
+++ src/literalParser.ts
@@ -2,19 +2,11 @@ import {Kind, ValueNode} from 'graphql/language';
 
 const createParseLiteral = (coerceType: (value: unknown) => string | void): (ast: ValueNode) => string | void => {
 	return (ast: ValueNode): string | void => {
-		if (ast.kind === Kind.STRING) {
-			try {
-				return coerceType(ast.value);
-			} catch (error) {
-				if (error instanceof TypeError) {
-					return undefined;
-				}
-
-				throw error;
-			}
+		if (ast.kind !== Kind.STRING) {
+			return undefined;
 		}
 
-		return undefined;
+		return coerceType(ast.value);
 	};
 };
 
